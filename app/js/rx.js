4b0e885acd6e037eed41cc514fdce9ab1debfd9b
ilazarte <ivan.lazarte@gmail.com>
2015-06-28 03:42:38 -0400

    intellij settings and simple rx wip

--- app/js/rx.js
+++ app/js/rx.js
@@ -1,10 +1,22 @@
-var svg = d3.select("svg");
+'use strict';
+
+var svg = d3.select('svg');
 
 function observables() {
-  
+
+  var source = Rx.DOM.mousedown(svg.node());
+
+  source.subscribe(
+    function (x) {
+      console.log('clicked!', x);
+    },
+    function (err) {
+      console.log('Error: ' + err);
+    },
+    function () {
+      console.log('Completed');
+    });
 
 }
 
-function main() {
-  observables();
-}
+observables();
