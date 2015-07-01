97d86efc83a7d159e2e18d88b55cd1cf40bdaf6f
ilazarte <ivan.lazarte@gmail.com>
2015-07-01 01:10:17 -0400

    adding babel as compiled language

--- app/babel/rx.js
+++ app/babel/rx.js
@@ -2,35 +2,19 @@
 
 var svg = d3.select('svg');
 
-class Node {
-  constructor(parent, name) {
-    this.node = d3.select(parent).append(name);
-    this.name = name;
-  }
-
-  exists() {
-    return this.node ? true : false;
-  }
-
-  remove() {
-    d3.select(this.node).remove();
-  }
-
-  toString() {
-    return this.name;
-  }
-}
+let logger = x => console.log('event: ', x),
+  completed = () => R.partial(logger, "completed");
 
 function observables() {
 
-  let source = Rx.DOM.mousedown(svg.node()),
-    log = x => console.log('clicked', x),
-    err = e => console.log(e),
-    com = () => console.log("completed"),
-    node = new Node(svg, "div");
+  let source = Rx.DOM.mousemove(svg.node())
+    .map(event => ({
+      x: event.clientX,
+      y: event.clientY
+    }))
+    .bufferWithCount(5);
 
-  source.subscribe(log, err, com);
-  console.log("the output of my node value", node);
+  source.subscribe(logger, null, completed);
 }
 
 observables();
